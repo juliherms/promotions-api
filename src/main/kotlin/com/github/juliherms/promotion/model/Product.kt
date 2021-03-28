32a3a6c3ed3589f1dcef8b1d1e1000daa6bd8f05
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-03-28 11:12:55 -0300

    create entity

--- src/main/kotlin/com/github/juliherms/promotion/model/Product.kt
+++ src/main/kotlin/com/github/juliherms/promotion/model/Product.kt
@@ -1,6 +1,16 @@
 package com.github.juliherms.promotion.model
 
+import javax.persistence.*
+
+/**
+ * This class represents product in the system
+ */
+@Entity
+@Table(name = "TB_PRODUCT")
 data class Product(
-        val id: Long,
-        val description: String
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 1L,
+        val description: String = ""
 )
