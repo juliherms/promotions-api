81345a298125b9307a00d5646db2a90afb425a37
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-04-02 09:54:00 -0300

    configure initial load with data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_product (name, description) VALUES ('name1', 'description1');
+INSERT INTO tb_product (name, description) VALUES ('name2', 'description2');
+INSERT INTO tb_product (name, description) VALUES ('name3', 'description3');
